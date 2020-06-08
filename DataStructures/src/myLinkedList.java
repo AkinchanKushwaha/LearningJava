02a8b0a9f1ffcab1a26fd5998fc52c474d8a4a71
AkinchanKushwaha <66404345+AkinchanKushwaha@users.noreply.github.com>
2020-06-08 16:04:54 +0530

    Implementation of singly linked list.

--- DataStructures/src/myLinkedList.java
+++ DataStructures/src/myLinkedList.java
@@ -3,7 +3,7 @@
 public class myLinkedList<E> {
 	Node<E> head;
 	
-	void add(E data) {
+	public void add(E data) {
 		Node<E> toAdd = new Node<>(data);
 		if(head == null) {
 			head = toAdd;
@@ -16,6 +16,8 @@ public class myLinkedList<E> {
 		temp.next = toAdd;
 	
 	}
+	
+	
 	void print() {
 		Node<E> temp = head;
 		while(temp != null) {
@@ -23,8 +25,48 @@ public class myLinkedList<E> {
 			temp = temp.next;
 		}
 	}
-	boolean isEmpty() {
+	
+	
+	public boolean isEmpty() {
 		return head == null;
+	}
+	
+	
+	public E removeLast() throws Exception{
+		
+		Node<E> temp = head;
+		
+		if(temp==null) {
+			throw new Exception("Cannot remove last element from last.");
+		}
+		if(temp.next== null) {
+			Node<E> toRemove = head;
+			head = null;
+			return toRemove.data; 
+			
+		}
+		while(temp.next.next !=null) {
+			temp = temp.next;
+		}
+		Node<E> toRemove = temp.next;
+		temp.next = null;
+		return toRemove.data;
+	}
+	
+	
+	public E getLast() throws Exception{
+		Node<E> temp = head;
+		
+		if(temp==null) {
+			throw new Exception("Cannot peek last element from last.");
+		}
+		
+		while(temp.next !=null) {
+			temp = temp.next;
+		}
+		return temp.data;
+
+		
 	}
 	static class Node<E> {
 		E data;
