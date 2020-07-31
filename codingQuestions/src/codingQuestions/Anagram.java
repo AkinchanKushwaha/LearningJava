5372b393aa7a9662c40e749fb2933ff6c0784968
AkinchanKushwaha <akinchandroger@gmail.com>
2020-07-31 12:08:21 +0530

    to check if two string are anagram of each other.

--- codingQuestions/src/codingQuestions/Anagram.java
+++ codingQuestions/src/codingQuestions/Anagram.java
@@ -1,5 +1,39 @@
 package codingQuestions;
 
+import java.util.Scanner;
+
 public class Anagram {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first String: ");
+		String a = sc.next();
+		System.out.println("Enter second String: ");
+		String b = sc.next();
+		int[] x = new int[256];
+		boolean isAnagram = true;
+		for(char c : a.toLowerCase().toCharArray()) {
+			int index = (int) c;
+			x[index]++;
+			
+		}
+		for(char c : b.toLowerCase().toCharArray()) {
+			int index = (int)c;
+			x[index]--;
+		}
+		for(int i = 0 ; i<256; i++) {
+			if(x[i]!=0) {
+				isAnagram = false;
+				break;
+			}
+			
+			
+		}
+		if(isAnagram)
+			System.out.println("Anagram !");
+		else
+			System.out.println("Not Anagram ! ");
+		
+		
+	}
 
 }
