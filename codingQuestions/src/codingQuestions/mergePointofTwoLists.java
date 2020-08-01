273cc823bf78de84584c4d5c0de7a4f8d550d632
AkinchanKushwaha <akinchandroger@gmail.com>
2020-08-01 16:41:16 +0530

    hackerrank

---
+++ codingQuestions/src/codingQuestions/mergePointofTwoLists.java
@@ -0,0 +1,92 @@
+package codingQuestions;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class mergePointofTwoLists {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the findMergeNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+//    ==========ANSWER=========
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        
+        SinglyLinkedListNode temp1 = head1;
+        SinglyLinkedListNode temp2 = head2;
+
+        while(temp1 != temp2){
+            if(temp1.next == null){
+                temp1.next = head2;
+            }
+            else{
+                temp1 =  temp1.next;
+            }
+            if(temp2.next == null){
+                temp2.next = head1;
+            }
+            else{
+                temp2 = temp2.next;
+                }
+            }
+        
+       return temp1.data;
+    }
+//    ============================
+}
