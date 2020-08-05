17412f0237a0b68127a7b3f0e0bba144a298eae9
AkinchanKushwaha <akinchandroger@gmail.com>
2020-08-06 00:05:18 +0530

    HackerRank

---
+++ codingQuestions/src/HackerRank/HeightOfABinaryTree.java
@@ -0,0 +1,35 @@
+package HackerRank;
+
+import java.util.*;
+import java.io.*;
+
+
+
+class HeightOfABinaryTree {
+
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static int height(Node root) {
+      	// Write your code here.
+        if(root == null) return -1;
+        
+        return Math.max(height(root.left), height(root.right))+1;
+    }
+
+	class Node {
+	    Node left;
+	    Node right;
+	    int data;
+	    
+	    Node(int data) {
+	        this.data = data;
+	        left = null;
+	        right = null;
+	    }
+	}
+
+}
