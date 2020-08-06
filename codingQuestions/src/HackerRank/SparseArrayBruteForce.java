5e6f410ea03a6d5b5329d20fb497ce1c149cc37a
AkinchanKushwaha <akinchandroger@gmail.com>
2020-08-06 21:00:19 +0530

    HackerRank

--- codingQuestions/src/HackerRank/SparseArrayBruteForce.java
+++ codingQuestions/src/HackerRank/SparseArrayBruteForce.java
@@ -1,5 +1,71 @@
 package HackerRank;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class SparseArrayBruteForce {
 
+    // Complete the matchingStrings function below.
+	
+	// ====Brute Force Method======
+    static int[] matchingStrings(String[] strings, String[] queries) {
+    	int [] n = new int[queries.length];
+    	for(int i = 0 ; i< queries.length; i++) {
+    		for(int j = 0 ; j<strings.length; j ++) {
+    			if(queries[i].compareTo(strings[j]) == 0) {
+    				n[i]++;
+    			}
+    		}
+    		
+    	}
+    	return n;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringsCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] strings = new String[stringsCount];
+
+        for (int i = 0; i < stringsCount; i++) {
+            String stringsItem = scanner.nextLine();
+            strings[i] = stringsItem;
+        }
+
+        int queriesCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] queries = new String[queriesCount];
+
+        for (int i = 0; i < queriesCount; i++) {
+            String queriesItem = scanner.nextLine();
+            queries[i] = queriesItem;
+        }
+
+        int[] res = matchingStrings(strings, queries);
+
+        for (int i = 0; i < res.length; i++) {
+            bufferedWriter.write(String.valueOf(res[i]));
+
+            if (i != res.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
