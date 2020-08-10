153ba2ec747078325715b0a8a5fa003646560a43
AkinchanKushwaha <akinchandroger@gmail.com>
2020-08-10 23:21:45 +0530

    HackerRank

---
+++ codingQuestions/src/ThirtyDaysOfCodeHackerRank/daySixteen.java
@@ -0,0 +1,22 @@
+package ThirtyDaysOfCodeHackerRank;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class daySixteen {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+
+        try{
+            int a = Integer.parseInt(S);
+            System.out.println(a);
+        }catch(Exception e){
+            System.out.print("Bad String");
+        }
+    }
+}
