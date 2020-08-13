4b6c3ea4f75094b27f54cc099884a8e7c9be4923
AkinchanKushwaha <akinchandroger@gmail.com>
2020-08-13 06:47:08 +0530

    Implementation of Queue.

--- DataStructures/src/myQueue.java
+++ DataStructures/src/myQueue.java
@@ -1,53 +1,68 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
-
-public class myQueue<E> {
-
-	Node<E> head;
-	Node<E> rear;
+class myQueue<E>{
 	
+	private Node<E> last, first;
 	
-	public void enqueue(E e) {
-		Node<E> toAdd = new Node<>(e);
-		if(head == null) {
-			head =rear = toAdd;
-			return;
-		}
-		rear.next = toAdd;
-		rear = rear.next;
+	public void add(E item) {
+		Node<E> toAdd = new Node<>(item);
 		
+		if(first == null) {
+			first = last = toAdd;
+		}
+		last.next = toAdd;
+		last = last.next;
+	}
+	
+	public E remove() {
+		if(first == null) throw new NoSuchElementException();
+		Node<E> toRemove = first;
+		first = first.next;
+		if(first == null) {
+			last = null;
+		}
+		return toRemove.data;
 		
 	}
-	public E dequeue() {
-		
-		if(head == null) {
-			return null;
-		}
-		
-		Node<E> temp = head ;
-		head=head.next;
-		
-		if(head == null) {
-			rear= null;
-		}
-		return temp.data;
-		
+	
+	public E peek() {
+		if(first == null) throw new NoSuchElementException();
+		return first.data;
 	}
-	public static class Node<E> {
+	
+	public boolean isEmpty() {
+		return first == null;
+	}
+	
+	public void print() {
+		Node<E> temp =first ;
+		while(temp !=null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+	
+	public static class Node<E>{
 		E data;
 		Node<E> next;
-		public Node(E data) {
+		public Node(E data){
 			this.data = data;
 			next = null;
 		}
-		
-
 	}
-	public static void main(String [] args) {
-		myQueue<Integer> mq = new myQueue<>();
-		mq.enqueue(21);
-		System.out.println(mq.dequeue());
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		myQueue<Integer> queue = new myQueue<>();
+		for(int i = 0 ; i< 5; i++) {
+			queue.add(i);
+		}
+		for(int i = 0 ; i< 5; i++) {
+			System.out.println(queue.remove());
+		}
+		sc.close();
+//		queue.print();
 		
-		System.out.println(mq.dequeue());
 	}
-
 }
